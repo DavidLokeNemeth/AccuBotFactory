b5e600f8832bd007b88a1f678d1d59d1afed3b1d
David Nemeth <david.nemeth@healthcareinternational.com>
2023-10-13 13:07:17 +0100

    Models and Migrations setup

--- database/migrations/2023_10_13_110901_create_components_table.php
+++ database/migrations/2023_10_13_110901_create_components_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('components', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('sku')->unique();
+            $table->text('description');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->decimal('weight', 5, 2);
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('components');
     }
 };
